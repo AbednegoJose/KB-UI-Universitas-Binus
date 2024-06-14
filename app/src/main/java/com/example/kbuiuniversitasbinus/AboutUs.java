5d8a65d02727dcfefe5100a7326615b1fdc3d559
AbednegoJose <322010016@student.machung.ac.id>
2024-06-14 13:21:59 +0700

    Home Page selesai

--- app/src/main/java/com/example/kbuiuniversitasbinus/AboutUs.java
+++ app/src/main/java/com/example/kbuiuniversitasbinus/AboutUs.java
@@ -1,8 +1,6 @@
 package com.example.kbuiuniversitasbinus;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutUs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_us);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void About(View view) {
-        Intent intent = new Intent(MainActivity.this, AboutUs.class);
-        startActivity(intent);
     }
 }

9bbb11c716ae0105575667374019b087b2364625
AbednegoJose <322010016@student.machung.ac.id>
2024-06-14 16:37:31 +0700

    5 Page dan Intent

--- app/src/main/java/com/example/kbuiuniversitasbinus/AboutUs.java
+++ app/src/main/java/com/example/kbuiuniversitasbinus/AboutUs.java
@@ -1,6 +1,8 @@
 package com.example.kbuiuniversitasbinus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class AboutUs extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void ContactUs(View view) {
+        Intent intent = new Intent(AboutUs.this, ContactUs.class);
+        startActivity(intent);
     }
 }
